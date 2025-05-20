d593bcc970e04ba71fa87eebeaba294e72240200
WHTech <93919489+WHTechno@users.noreply.github.com>
2025-05-20 13:52:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-module.exports = {
-  fs: require('./constants/fs'),
-  os: require('./constants/os'),
-  errno: require('./constants/errno')
+const log = {
+  success: (msg) => console.log(`✅ ${msg}`),
+  error: (msg) => console.error(`❌ ${msg}`),
+  warn: (msg) => console.warn(`⚠️ ${msg}`),
+  info: (msg) => console.info(`ℹ️ ${msg}`)
 };
+
+module.exports = log;
